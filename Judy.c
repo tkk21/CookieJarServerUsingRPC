9a277fa8c3400c0cb7920662545e9582fe3302b3
tkk21 <tkk21@case.edu>
2015-11-20 15:26:33 -0500

    implemented judy

--- Judy.c
+++ Judy.c
@@ -1,3 +1,41 @@
 #include "Cookie.h"
+#include "Helpers.h"
+
+int main (int argc, char**argv){
+    CLIENT *client;
+    int *a;
+    char *server;
+    
+    if (argc !=2 ){
+        fprintf(stderr, "Please type argument so that %s HOSTNAME\n", *argv);
+        return 1;
+    }
+    server = argv[1];
+    
+    //make the client
+    if ( (client = clnt_create(server, DISPLAY_PRG, DISPLAY_VER, "udp")) == (CLIENT *)NULL){
+        clnt_pcreateerror(server);
+        exit(2);
+    }
+
+    while (*a != -2){
+       print_info(); printf("Request for a cookie\n");
+       struct CookieRequest request = {JudyNum};
+       a = request_cookie_1();
+
+       if (*a == 1){
+           print_info(); printf("I got a cookie <:\n");
+       
+       }
+       else if (*a == -1){
+           print_info(); printf("I didn't get a cookie ;-;\n");
+       
+       }
+       else if (*a == -3) {
+            fprintf(stderr, "invalid b value sent to the server\n");
+       }
+    }
 
 
+
+}
